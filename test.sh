9b551f280ce21a826237cac43d67710b61ec7a20
quentin jadeau <quentin.jadeau@epitech.eu>
2016-09-09 01:28:21 +0200

    Adjuste style, fix remove

---
+++ test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+doiuse -b "ie >= 10, chrome >= 48, ff >= 45" css/style.css -i "flexbox, multicolumn, font-unicode-range, pointer-events, css-touch-action, pointer, css-appearance, css-filters, text-size-adjust, transforms3d, text-size-adjust"
